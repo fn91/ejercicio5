67e793bfabceb9916d02942b2e6df9794fb268e4
fn91 <claudio>
2023-11-13 22:06:44 +0100

    Esto es el arreglo

--- opera_test.py
+++ opera_test.py
@@ -15,8 +15,8 @@ class TestMatematico(unittest.TestCase):
         self.assertEqual(suma(-1, -1),2)
         self.assertEqual(resta(-1, -99)-100)
         self.assertEqual(resta(99, 1),98)
-        self.assertEqual(multi(1, 10),10)
-        self.assertAlmostEqual(division(12,0),0)
+        self.assertEqual(multi(10, 0),0)
+        self.assertAlmostEqual(division(12,4)3)
         
 
 
